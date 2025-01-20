656c2fe68e26306de2392010770019b6bc4727c3
Mark Rudolph <mark@k8ty.app>
2025-01-19 23:12:06 -0500

    Update blarg.rb

--- Formula/blarg.rb
+++ Formula/blarg.rb
@@ -3,10 +3,11 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Blarg < Formula
   desc "Some sort of blog engine, maybe?"
-  homepage ""
+  homepage "https://github.com/wishingtreedev/blarg"
   url "https://github.com/wishingtreedev/blarg/releases/download/v0.0.2/blarg-linux-x64.zip"
   sha256 "40c74505704f4444a2a7ec027bdab49b16c17efc7141c58392d505ae7bfc89bd"
   license "Apache-2.0"
+  version "0.0.2"
 
 
   def install
